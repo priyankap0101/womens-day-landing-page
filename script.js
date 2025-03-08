320bc18eaf525f4e98fa80ac62a8cb97861b780c
priyanka <pandharbalepr1997@gmail.com>
2025-03-08 11:29:08 +0530

    removed counter

--- script.js
+++ script.js
@@ -9,19 +9,6 @@ function celebrate() {
   confetti({ particleCount: 100, spread: 70 });
 }
 
-function countdownTimer() {
-  const eventDate = new Date("March 8, 2025 00:00:00").getTime();
-  setInterval(() => {
-    let now = new Date().getTime();
-    let diff = eventDate - now;
-    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    document.querySelector(
-      ".countdown"
-    ).innerText = `🌸 ${days} days to Women's Day!`;
-  }, 1000);
-}
-countdownTimer();
-
 let slideIndex = 0;
 const slides = document.querySelectorAll(".carousel-slide");
 
